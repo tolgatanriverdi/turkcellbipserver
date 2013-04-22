8a5cb8667554b9b113bed2ca5fa62471a6d1cd0d
Tolga Tanriverdi <tolgatanriverdi@gmail.com>
2013-04-22 13:21:23 +0300

    test

--- register.php
+++ register.php
@@ -30,6 +30,7 @@ if (isset($userInput))  {
 		$userName = $userPhone."@".$xmppDomain;
 		$result = @mysql_query("SELECT * FROM users WHERE username='$username'") or die("Query Error");
 		$num = mysql_num_rows($result);
+		echo "Bu kadar kayit bulundu:".$num. "For Username: $username <br>";
 		
 		$currentDate = date("Y-m-d H:i:s",time());
 		$resultArr["result"] = 7; //Default database hatasi set edilir bir sorun cikmassa successe (0) cevrilir;
