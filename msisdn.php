aabc92b53ec7ee9d706d358a34c5565295058fcf
Tolga Tanriverdi <tolgatanriverdi@gmail.com>
2013-04-22 10:25:37 +0300

    msisdn ayarlari degisti

--- msisdn.php
+++ msisdn.php
@@ -7,9 +7,8 @@ $userInput = $_POST["userInput"];
 
 if (isset($userInput)) {
 	$inputArr = json_decode($userInput,true);
-	var_dump($inputArr);
 	$userPhone = $inputArr["msisdn"];
-	echo "Input Arr:".$inputArr." UserPhone:".$userPhone."User Input:".$userInput."<br>";
+	//echo "Input Arr:".$inputArr." UserPhone:".$userPhone."User Input:".$userInput."<br>";
 	$resultArr = array();
 	$resultArr["msisdn"] = $userPhone;
 	
