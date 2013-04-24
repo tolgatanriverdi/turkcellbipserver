2356ce7daa9849327dedc31fdefb7430c87f55f6
Tolga Tanriverdi <tolgatanriverdi@gmail.com>
2013-04-24 17:21:19 +0300

    urldecode

--- profileupload.php
+++ profileupload.php
@@ -45,7 +45,7 @@ $profileImage = $_FILES["file"];
 $resultArr = array();
 
 if (isset($userInput) && isset($profileImage)) {
-	$jsonInput = urldecode(json_decode($userInput,true));
+	$jsonInput = json_decode(urldecode($userInput),true);
 	$id = $jsonInput["id"];
 	$nickName = $jsonInput["nickname"];
 	//echo "User Input:".$userInput." ID:".$id." Nick:".$nickName."<br>";
