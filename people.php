73f64bd29e0b26fce28db490910aa256f407f19a
Tolga Tanriverdi <tolgatanriverdi@gmail.com>
2013-05-02 22:31:05 +0300

    userphone

--- people.php
+++ people.php
@@ -80,6 +80,7 @@ if (isset($userInput)) {
 						$result = @mysql_query("SELECT username FROM users WHERE id='$userID'") or die(json_encode($resultArr));
 						$row = mysql_fetch_row($result);
 						$userPhone = strstr($row[0], "@",true);
+						echo "<br> UserPhone: '$userPhone' <br>";
 						mysql_free_result($result);
 						
 						$value = substr($phoneUtil->format($number, PhoneNumberFormat::E164), 1);
