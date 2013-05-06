037e7840f4e1577a8ddbd0cdc16c68ec1d245b37
Tolga Tanriverdi <tolgatanriverdi@gmail.com>
2013-05-06 23:24:22 +0300

    upload server degistirildi

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $databaseAddr = "localhost";
 $databaseName = "turkcellbipserver";
 $databaseUser = "turkcellbip";
 $databaseUserPass = "bip123*";
-$uploadsDir = "/opt/lampp/htdocs/uploads";
+$uploadsDir = "/opt/lampp/htdocs/turkcellbipserver/uploads";
 $uploadsUrl = "http://tolgatanriverdi.dyndns.org/uploads/";
 
 $xmppUrl = "http://127.0.0.1:5285/rest";
